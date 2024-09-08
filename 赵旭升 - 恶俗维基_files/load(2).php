44e0ff0bae951b9ff7dd6771725331eabcdbb686
catreimu <2188372177@qq.com>
2024-09-08 17:41:20 +0800

    Add files via upload

---
+++ 赵旭升 - 恶俗维基_files/load(2).php
@@ -0,0 +1,21 @@
+table.navbox{border:1px solid #7384b5;width:100%!important;max-width:950px;margin:auto;clear:both;font-size:88%;text-align:center;padding:1px}table.navbox+table.navbox{margin-top:-1px}.navbox-title,.navbox-abovebelow,table.navbox th{text-align:center;padding-left:1em;padding-right:1em}.navbox-group{white-space:nowrap;text-align:right;font-weight:700;padding-left:1em;padding-right:1em}.navbox,.navbox-subgroup{background:#fdfdfd}.navbox-list{border-color:#fdfdfd;text-align:left}.navbox-title,table.navbox th{background:#cedaff}.navbox-abovebelow,.navbox-group,.navbox-subgroup .navbox-title{background:#dde6ff}.navbox-subgroup .navbox-group,.navbox-subgroup .navbox-abovebelow{background:#dde6ff}.navbox-even{background:#f7f7f7}.navbox-odd{background:0 0}.collapseButton{float:right;font-weight:400;text-align:right;width:auto}.navbox .collapseButton{width:6em}.navbox-title,.navbox-above,.navbox-below{padding-left:1em;padding-right:1em;text-align:center}.zhy-mobile,.mobile-block{display:none!important}.desktop-inline{display:inline}.imgbox img,.mw-parser-output img{max-width:100%;height:auto}body.ns-2 div#catlinks{display:none!important}#siteNotice{z-index:1}.grecaptcha-badge{visibility:hidden}html,body,.comment-body textarea,.comment-preview,.comment-submit,#firstHeading{font-family:helvetica neue,Tahoma,pingfang sc,microsoft yahei,微软雅黑,hiragino sans gb,wenquanyi micro hei,sans-serif}span[lang=ja]{font-family:BlinkMacSystemFont,-apple-system,helvetica neue,Tahoma,Arial,source han sans jp,noto sans cjk jp,source han sans,ヒラギノ角ゴ pro w3,hiragino kaku gothic pro,Osaka,メイリオ,Meiryo,ＭＳ Ｐゴシック,ms pgothic,sans-serif}
+/*
+     FILE ARCHIVED ON 12:59:37 Feb 17, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:28:35 Sep 08, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.427
+  exclusion.robots: 0.015
+  exclusion.robots.policy: 0.007
+  esindex: 0.008
+  cdx.remote: 6.3
+  LoadShardBlock: 95.871 (3)
+  PetaboxLoader3.datanode: 178.286 (5)
+  PetaboxLoader3.resolve: 97.793 (3)
+  load_resource: 191.18 (2)
+*/
